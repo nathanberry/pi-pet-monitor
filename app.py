775cf921af48e092cee956e9feb6957c900e6a26
Kailey Berry <kaileybberry@gmail.com>
2014-08-08 19:08:51 -0600

    Changed to use temperature instead of name

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+def get_current_temp():
+    return 70
+
+
 @app.route("/")
 def hello():
-    return render_template("index.html", name="Joe")
+    temp = get_current_temp()
+    return render_template("index.html", temp=temp)
 
 if __name__ == "__main__":
     app.run()
